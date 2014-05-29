4f179118225c4a5050dde083e3ba6ec060bfa162
Zach Levy <zacharyalevy@gmail.com>
2014-05-28 21:28:05 -0400

    Answers for palindrome and baseball

---
+++ assignment1/assignment1/answers.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class answers here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class answers
+{
+    public static void main(String[] args) {
+        System.out.println("Starting test\n");
+        
+        // question 12
+        // tests if the input string is a palindrome, shows a success
+        Palindrome successPalindrome = new Palindrome("racecar");
+        successPalindrome.isPalindrome();
+
+        // tests if the input string is a palindrome, shows a failure
+        Palindrome failurePalindrome = new Palindrome("apples");
+        failurePalindrome.isPalindrome();
+
+        // question 13
+        BaseballScore worldSeriesFinal = new BaseballScore();
+        worldSeriesFinal.setScores();
+        System.out.println("And the World Series result is... " + worldSeriesFinal.isWinner());
+    }
+}
