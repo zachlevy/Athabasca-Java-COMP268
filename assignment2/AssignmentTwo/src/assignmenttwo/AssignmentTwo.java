2cc443515e79b4aa796a0660cb485d4b9ec6096c
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-07-15 10:02:34 -0400

    assignment 2 init

---
+++ assignment2/AssignmentTwo/src/assignmenttwo/AssignmentTwo.java
@@ -0,0 +1,21 @@
+/*
+ * Answers for assignment 2
+ */
+
+package assignmenttwo;
+
+/**
+ *
+ * @author zacharylevy
+ * @version 20140715
+ */
+public class AssignmentTwo {
+    // instance vars
+
+    
+    // constructor
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ assignment2/readme.txt
@@ -0,0 +1,3 @@
+Assignment 2 was coded using NetBeans 8.0
+The AssignmentTwo.java class has all the example classes being run
+There is 1 class for each question
